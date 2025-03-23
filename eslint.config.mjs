b04a24bd3ef5c7c17a22d28376014b91e61be4fb
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-03-24 06:21:08 +0700

    style: add eslint rules, prettier, lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "no-console": "error",
+      "no-unused-vars": "error",
+      "no-var": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-alert": "error",
+      "no-nested-ternary": "error",
+      "consistent-return": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
