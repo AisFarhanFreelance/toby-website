b98d1228bae89adb9667b09760576af444fe4351
Aldi-H <aldihimawan88@gmail.com>
2025-03-25 18:35:00 +0700

    feat: add villa detail and listing pages with layout component

---
+++ src/app/villas/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function VillasLayout({ children }: { children: ReactNode }) {
+  return <div>{children}</div>;
+}
---
+++ src/app/villas/page.tsx
@@ -0,0 +1,9 @@
+const Villas = () => {
+  return (
+    <div>
+      <h1>Find A Villa</h1>
+    </div>
+  );
+};
+
+export default Villas;
