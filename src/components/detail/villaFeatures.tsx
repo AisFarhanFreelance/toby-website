95fe08f15a6be58c0a4d0d0e3fcf89dd8e9b5cad
Aldi-H <aldihimawan88@gmail.com>
2025-03-26 15:12:40 +0700

    feat: add BookingPrice and DescriptionAndPrice components; remove VillaFeatures component

--- src/components/detail/villaFeatures.tsx
+++
@@ -1,9 +0,0 @@
-const VillaFeatures = () => {
-  return (
-    <div>
-      <h1>Villa Features</h1>
-    </div>
-  );
-};
-
-export default VillaFeatures;
--- src/components/detail/villaOverview.tsx
+++ src/components/detail/villaOverview.tsx
@@ -4,6 +4,40 @@ import FacilitiesAndAmenities from "./facilitiesAndAmenities";
 
 import BedRoom from "/public/assets/images/BedRoom.png";
 import KeyFeature from "./keyFeature";
+import DescriptionAndPrice from "./descriptionAndPrice";
+
+const sections = [
+  {
+    subtitle: "Location",
+    content: [
+      "Welcome to our stunning villa in the heart of Jimbaran, Bali, precisely in Kedonganan. This exceptional property seamlessly blends Scandinavian charm with modern contemporary design, creating a tranquil and luxurious retreat for your vacation.",
+      "As you step into the villa, you'll be greeted by the soothing sounds of a small waterfall, setting the tone for a relaxing stay. The property spans two levels and features three beautifully appointed bedrooms, ensuring ample space and comfort for your entire party.",
+      "Escape to our villa in Jimbaran, where every detail is thoughtfully designed to create an unforgettable vacation experience. Immerse yourself in the perfect blend of Scandinavian aesthetics and contemporary comfort for an unparalleled stay in Bali.",
+    ],
+  },
+  {
+    subtitle: "The Villa",
+    content: [
+      "Upon entering, you'll find yourself in the spacious living room that opens up to a pristine pool area. The open kitchen and dining area enhance the sense of space and connectivity, providing the perfect setting for entertaining or enjoying a cozy meal with your loved ones.",
+      "Ascending to the second floor, you'll discover an open space serving as a sitting area and mini bar, perfect for unwinding and enjoying the breathtaking views of the surroundings. Whether you're sipping a cocktail or simply soaking in the ambiance, this space adds an extra layer of luxury to your experience.",
+    ],
+  },
+  {
+    subtitle: "The Bedroom",
+    content: [
+      "The master bedroom and the second bedroom are located on the first level, both designed with a touch of elegance and equipped with ensuite bathrooms featuring a bathtub, shower area, and a toilet area discreetly positioned behind the sink.",
+      "The second bedroom has a similar design to the master bedroom, boasting twin beds, and also offers a comfortable couch for added convenience. The room is located at the left of the building layout.",
+      "The third bedroom, located on the second level, provides a private and serene retreat, ensuring peace and tranquility during your stay. The consistency in bathroom design extends to this level, maintaining the theme of sophistication and functionality.",
+    ],
+  },
+  {
+    subtitle: "The Outdoor Area",
+    content: [
+      "The outdoor area is a true oasis, featuring a sparkling swimming pool, a charming gazebo for moments of relaxation, an inviting outdoor dining area, and a barbecue space for delightful al fresco meals.",
+      "Whether you're taking a refreshing dip in the pool or enjoying a barbecue under the stars, the outdoor amenities complement the villa's allure.",
+    ],
+  },
+];
 
 interface VillaOverviewProps {
   villaName: string;
@@ -17,6 +51,7 @@ const VillaOverview = ({ villaName }: VillaOverviewProps) => {
         <Image src={BedRoom} alt="BedRoom" fill className="object-cover" />
       </div>
       <KeyFeature />
+      <DescriptionAndPrice sections={sections} />
     </div>
   );
 };
