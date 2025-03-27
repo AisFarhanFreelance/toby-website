12c6eff153cb4c3f3152986629c93d204f0ced1a
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-03-27 16:18:11 +0700

    feat: integrate list villa with supabase

--- src/app/villas/page.tsx
+++ src/app/villas/page.tsx
@@ -1,9 +1,28 @@
 import FindAVilla from "@/components/list/findAVilla";
+import { createClient } from "@/lib/utility/supabase/server";
 
-const Villas = () => {
+const Villas = async () => {
+  const supabase = await createClient();
+
+  const { data } = await supabase.from("villas").select(`
+    id,
+    name,
+    short_address,
+    price,
+    rating,
+    capacity,
+    bedroom,
+    bathroom,
+    area,
+    description,
+    review_count,
+    images (url),
+    tags (tag)
+  `);
+
   return (
     <div>
-      <FindAVilla />
+      <FindAVilla villas={data as villa[]} />
     </div>
   );
 };
